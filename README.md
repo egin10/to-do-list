cc70da939a4b9d2e69c6e1e42eedc445ac2abe8a
egin10 <egin066@gmail.com>
2017-07-01 17:27:57 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # to-do-list
 Learn Expressjs, Mysql for ToDo List
+
+1. clone : `git clone https://github.com/egin10/to-do-list`
+2. install : `cd to-do-list && npm i`
+3. run : `node app` or `npm start`
+
+Explore it!
---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var path = require('path');
+var swig = require('swig');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var app = express();
+
+//============= VIEW ENGINE
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+
+//============= MIDDLEWARE
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(session({
+  secret : 'SuperPower',
+  resave : false,
+  saveUninitialized : false
+}));
+
+//============= ROUTER
+app.get('/', function(req, res){
+  res.end('Hello World!');
+});
+
+
+//============= MIDDLEWARE 404 ROUTE
+app.use('*', function(req, res){
+  res.writeHead(404, {"Content-Type":"text/html"});
+  res.end('Page Not Found!');
+});
+
+var port = process.env.PORT || 8000;
+app.listen(port, function(req, res){
+  console.log('http://localhost:'+port+'/');
+});
